7d4aef141a0290d3d46b7168c66f0e09cea0eb22
Thomas Barregren <thomas@kntnt.com>
2023-09-21 20:57:01 +0200

    Initial coomit. Fully tested and working theme.

---
+++ patterns/header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Header
+ * Slug: kntnt-canvas/header
+ * Description: Header with logo, site name, call to action button, and hamburger menu with navigation and search.
+ * Categories: header
+ * Keywords: 
+ * Viewport Width: 1280
+ * Block Types: 
+ * Post Types: wp_template, wp_template_part
+ * Inserter: true
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|15","bottom":"var:preset|spacing|15"},"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|white-0"},":hover":{"color":{"text":"var:preset|color|white-0"}}}}},"backgroundColor":"primary-500","textColor":"white-0","layout":{"type":"constrained"},"fontSize":"16"} -->
+<div class="wp-block-group alignfull has-white-0-color has-primary-500-background-color has-text-color has-background has-link-color has-16-font-size" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--15);padding-bottom:var(--wp--preset--spacing--15)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":40,"shouldSyncIcon":false,"className":"is-style-default","style":{"color":[]}} /-->
+
+<!-- wp:site-title {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1"}},"className":"no-hover-underline no-link-color","fontSize":"24"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"left":"var:preset|spacing|15","right":"var:preset|spacing|15","top":"var:preset|spacing|5","bottom":"var:preset|spacing|5"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="padding-top:var(--wp--preset--spacing--5);padding-right:var(--wp--preset--spacing--15);padding-bottom:var(--wp--preset--spacing--5);padding-left:var(--wp--preset--spacing--15)"><?php _e( 'Contact Us', 'kntnt-canvas'); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:navigation {"ref":18,"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"secondary-800","overlayTextColor":"white-0","className":"has-shadow","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"left","orientation":"horizontal"}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== Kntnt Canvas ===
+Contributors: tbarregren
+Requires at least: 6.0
+Tested up to: 6.3
+Requires PHP: 7.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+== Description ==
+
+Kntnt Canvas is a WordPress block theme that enables full site editing (FSE).
+
+Use it on its own or as a parent theme for [Kntnt Canvas Child](https://github.com/Kntnt/kntnt-canvas-child).
+
+Kntnt Canvas is developed to accomplish three objectives:
+
+1. It should be a beautiful and usable theme out of the box.
+2. It should serve as a starting point from which more customized themes can be developed in the WordPress site editor.
+3. It should not reset the browser's built-in style sheets or override WordPress default CSS unless absolutely necessary. This is in order (i) to make it easier for developers to create custom CSS, (ii) to load as little CSS as possible, and (iii) to be as robust as possible against future changes and additions to the WordPress CSS.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Copyright ==
+
+Kntnt Canvas
+Copyright © Kntnt Sweden AB
+This WordPress theme is distributed under the GNU General Public License, Version 3 or later.
+License URL: http://www.gnu.org/licenses/gpl-3.0.html
+-- End of Kntnt Canvas credits --
+
+Inter Font
+Copyright © 2020 The Inter Project Authors 
+This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: http://scripts.sil.org/OFL
+License URL: http://scripts.sil.org/OFL 
+Source: https://rsms.me/
+-- End of Inter Font credits --
+
+Lora Font
+Copyright 2011 The Lora Project Authors (https://github.com/cyrealtype/Lora-Cyrillic), with Reserved Font Name "Lora". 
+This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL 
+License URL: https://scripts.sil.org/OFL 
+Source: https://cyreal.org
+-- End of Lora Font credits --
+
+JetBrains Mono Font
+Copyright 2020 The JetBrains Mono Project Authors (https://github.com/JetBrains/JetBrainsMono) 
+This Font Software is licensed under the SIL Open Font License, Version 1.1. This license is available with a FAQ at: https://scripts.sil.org/OFL 
+License URL: https://scripts.sil.org/OFL 
+Source: https://www.jetbrains.com
+-- End of JetBrains Mono Font credits --
